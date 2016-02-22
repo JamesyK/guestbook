bb40a0479db041953ff6c9cca5e1f1097ed6a2e0
JamesyK <jameskanegae@gmail.com>
2016-02-22 03:14:03 -0800

    finished styling for the night

--- application/views/guestbook/form.php
+++ application/views/guestbook/form.php
@@ -1,16 +1,18 @@
-<?php echo validation_errors(); ?>
+<div class="form">
+	<?php echo validation_errors(); ?>
 
-<?php echo form_open(); ?>
+	<?php echo form_open(); ?>
 
-    <label for="name">Name</label>
-    <input type="input" name="name" /><br />
+	    <label for="name">Name:</label>
+	    <input type="input" name="name" /><br />
 
-    <label for="email">Email</label>
-    <input type="input" name="email" /><br />
+	    <label for="email">Email:</label>
+	    <input type="input" name="email" /><br />
 
-    <label for="comment">Comment</label>
-    <textarea name="comment"></textarea><br />
+	    <label for="comment">Comment:</label>
+	    <textarea name="comment"></textarea><br />
 
-    <input type="submit" name="submit" value="Write In The Guestbook" />
+	    <input type="submit" name="submit" value="Submit" />
 
-</form>
+	</form>
+</div>
--- application/views/guestbook/index.php
+++ application/views/guestbook/index.php
@@ -1,16 +1,22 @@
-<?php foreach ($posts as $post): ?>
+<div class="posts">
+	<?php foreach ($posts as $post): ?>
 
-	<div class="main">
-		<div>
-			<?php echo $post['name']; ?> - 
-			<?php echo $post['email']; ?>
-			<span>
-				<?php echo anchor(base_url() . "index.php/guestbook/delete_post?id=" . $post['id'], 'DELETE', 'id="$row->id"'); ?>
-			</span>
+		<div class="post">
+			<div>
+				<span class="name">
+					<?php echo $post['name']; ?>
+				</span> - 
+				<span class="email">
+					<?php echo $post['email']; ?>
+				</span>
+				<span class="delete">
+					<?php echo anchor(base_url() . "index.php/guestbook/delete_post?id=" . $post['id'], 'X', 'id="$row->id"'); ?>
+				</span>
+			</div>
+			<div class="comment">
+				<?php echo $post['comment']; ?>
+			</div>
 		</div>
-		<div>
-			<?php echo $post['comment']; ?>
-		</div>
-	</div>
 
-<?php endforeach; ?>
+	<?php endforeach; ?>
+</div>
